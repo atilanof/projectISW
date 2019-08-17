f63674c7a4ac37e6d93dd80cff6dcce4266aa3ab
atilanof <atilanof@atilanof-work>
2019-08-17 19:31:04 +0200

    First version

---
+++ src/icai/dtc/isw/client/Client.java
@@ -0,0 +1,63 @@
+package icai.dtc.isw.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+	public static void main(String args[]) {
+		String host = "127.0.0.1";
+		int port = 8081;
+		new Client(host, port);
+	}
+
+	public Client(String host, int port) {
+		try {
+			String serverHostname = new String("127.0.0.1");
+
+			System.out.println("Connecting to host " + serverHostname + " on port " + port + ".");
+
+			Socket echoSocket = null;
+			PrintWriter out = null;
+			BufferedReader in = null;
+
+			try {
+				echoSocket = new Socket(serverHostname, 8081);
+				out = new PrintWriter(echoSocket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+			} catch (UnknownHostException e) {
+				System.err.println("Unknown host: " + serverHostname);
+				System.exit(1);
+			} catch (IOException e) {
+				System.err.println("Unable to get streams from server");
+				System.exit(1);
+			}
+
+			/** {@link UnknownHost} object used to read from console */
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+			while (true) {
+				System.out.print("client: ");
+				String userInput = stdIn.readLine();
+				/** Exit on 'q' char sent */
+				if ("q".equals(userInput)) {
+					break;
+				}
+				out.println(userInput);
+				System.out.println("server: " + in.readLine());
+			}
+
+			/** Closing all the resources */
+			out.close();
+			in.close();
+			stdIn.close();
+			echoSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/icai/dtc/isw/server/SocketServer.java
@@ -0,0 +1,73 @@
+package icai.dtc.isw.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer extends Thread {
+	public static final int PORT_NUMBER = 8081;
+
+	protected Socket socket;
+
+	private SocketServer(Socket socket) {
+		this.socket = socket;
+		System.out.println("New client connected from " + socket.getInetAddress().getHostAddress());
+		start();
+	}
+
+	public void run() {
+		InputStream in = null;
+		OutputStream out = null;
+		try {
+			in = socket.getInputStream();
+			out = socket.getOutputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String request;
+			while ((request = br.readLine()) != null) {
+				System.out.println("Message received:" + request);
+				request+=" by Atilano";
+				request += '\n';
+				out.write(request.getBytes());
+			}
+
+		} catch (IOException ex) {
+			System.out.println("Unable to get streams from client");
+		} finally {
+			try {
+				in.close();
+				out.close();
+				socket.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("SocketServer Example");
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(PORT_NUMBER);
+			while (true) {
+				/**
+				 * create a new {@link SocketServer} object for each connection
+				 * this will allow multiple client connections
+				 */
+				new SocketServer(server.accept());
+			}
+		} catch (IOException ex) {
+			System.out.println("Unable to start server.");
+		} finally {
+			try {
+				if (server != null)
+					server.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
